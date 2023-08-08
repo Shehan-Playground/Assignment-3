73fe0635f20ef81f2e543b26dcefae921e627733
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-08-08 06:50:00 +0530

    Custom validations

--- studentDetails.java
+++ studentDetails.java
@@ -2,20 +2,16 @@ import java.util.Scanner;
 
 public class studentDetails {
 
-    public static boolean nameValidate(String name) {
-        return true;
-    }
-
-    public static boolean ageValidate(int age) {
-        return true;
-    }
-
-    public static boolean subjectValidate(String subjectNames) {
-        return true;
-    }
-
-    public static boolean marksValidate(double subjectMark) {
-        return true;
+    public static void tableLines () {
+        
+        System.out.print("+");
+        System.out.print("-".repeat(14));
+        System.out.print("+");
+        System.out.print("-".repeat(14));
+        System.out.print("+");
+        System.out.print("-".repeat(21));
+        System.out.print("+");
+        System.out.println();
     }
 
     public static void display(String name, int age, String[] subjectNames, double[] subjectMarks) {
@@ -23,32 +19,35 @@ public class studentDetails {
         final String COLOR = "\033[3";
         final String RESET = "\033[0m";
         final String BOLD = "\033[1m";
-        
-        // final String TABLE = "| %-13s| %-13s| %-17s |\n";
 
         // Styling the name
+
         name = name.toUpperCase();
         name = COLOR+"4m"+BOLD+name+RESET;
 
         // Styling the age
+
         String ageDisplay = age+" years old";
 
         // Calculating the total and the average of marks
+
         double total = subjectMarks[0] + subjectMarks[1] + subjectMarks[2];
         double avg = total / 3;
         String avgString = String.format("%.2f", avg)+"%";
 
         // Styling the status
+
         String status;
         if (avg > 75) status = COLOR+"4m"+BOLD+"Distinguish Pass"+RESET;
 
         else if (avg>64) status = COLOR+"2m"+BOLD+"Credit Pass"+RESET;
     
-        else if (avg >54) status = COLOR+"3m"+BOLD+"ail"+RESET;
+        else if (avg >54) status = COLOR+"3m"+BOLD+"Pass"+RESET;
 
-        else status = COLOR+"1m"+BOLD+"Pass"+RESET;
+        else status = COLOR+"1m"+BOLD+"Fail"+RESET;
 
         //Checking the status of individual marks
+
         String[] statusMarks = new String [3];
 
         for (int itr=0; itr<3; itr++) {
@@ -63,12 +62,14 @@ public class studentDetails {
         }
 
         // Styling the subject name
+
         for (int itr=0; itr<3; itr++) {
 
             subjectNames[itr] = subjectNames[itr].substring(0,3)+String.format("%03d", Integer.parseInt(subjectNames[itr].substring(3)));
         }
 
-        // Display the result
+        // Display the result with string formatting
+
         System.out.println();
         System.out.print("+");
         System.out.print("-".repeat(51));
@@ -80,38 +81,17 @@ public class studentDetails {
         System.out.printf("| Status: %-55s|\n",status);
         System.out.printf("| Total: %-15.2f Avg: %-22s|\n",total, avgString);
 
-        System.out.print("+");
-        System.out.print("-".repeat(14));
-        System.out.print("+");
-        System.out.print("-".repeat(14));
-        System.out.print("+");
-        System.out.print("-".repeat(21));
-        System.out.print("+");
-        System.out.println();
+        tableLines();
 
-        System.out.printf("| %-13s| %-13s| %-19s |\n", "Subject", "Marks", "Status");
+        System.out.printf("| %-21s| %-21s| %-27s |\n", BOLD+"Subject"+RESET, BOLD+"Marks"+RESET, BOLD+"Status"+RESET);
 
-        System.out.print("+");
-        System.out.print("-".repeat(14));
-        System.out.print("+");
-        System.out.print("-".repeat(14));
-        System.out.print("+");
-        System.out.print("-".repeat(21));
-        System.out.print("+");
-        System.out.println();
+        tableLines();
         
         for (int itr=0; itr < 3; itr++) {
             System.out.printf("| %-13s| %-13.2f| %-32s |\n", subjectNames[itr], subjectMarks[itr], statusMarks[itr]);
         }
 
-        System.out.print("+");
-        System.out.print("-".repeat(14));
-        System.out.print("+");
-        System.out.print("-".repeat(14));
-        System.out.print("+");
-        System.out.print("-".repeat(21));
-        System.out.print("+");
-        System.out.println();
+        tableLines();
     }
 
 
@@ -163,7 +143,8 @@ public class studentDetails {
                 String subjectName = scanner.nextLine();
 
                 // Subject name input validation
-                if (!subjectName.startsWith("SE-")) {
+                subjectName = subjectName.strip();
+                if (!subjectName.startsWith("SE-") || (subjectName.length() == 3 || subjectName.substring(3).length() >3)) {
 
                     validity = false;
                     System.out.println("Invalid input subject name");
@@ -189,15 +170,20 @@ public class studentDetails {
 
         }
 
-        name = "Shehan rathnayake";
-        age = 13;
-        subjectNames[0] = "SE-1";
-        subjectNames[1] = "SE-2";
-        subjectNames[2] = "SE-3";
+        testData:
+        {
+            // if (true) break testData;
+            name = "Shehan rathnayake";
+            age = 13;
+            subjectNames[0] = "SE-1";
+            subjectNames[1] = "SE-2";
+            subjectNames[2] = "SE-3";
 
-        subjectMarks[0] = 88.523;
-        subjectMarks[1] = 68;
-        subjectMarks[2] = 40;
+            subjectMarks[0] = 88.523;
+            subjectMarks[1] = 68;
+            subjectMarks[2] = 40;
+        }
+        
         
         // Calling display finction to display the result
         if (validity) display(name, age, subjectNames, subjectMarks);
